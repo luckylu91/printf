13f5d53e7ebe624108d6350cb46f811f9a994d82
Lucas Zins <zins.lucas@gmail.com>
2021-05-31 14:46:57 +0200

    kk

--- test_caca.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("%p\n", 1000);
-	printf("%p\n", 10000000000l);
-}
--- test_printfc.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("| return = %d\n", printf("%c", '\x00'));
-	printf("| return = %d\n", printf("%5c", '\x00'));
-	printf("| return = %d\n", printf("%5c", 'a'));
-	printf("| return = %d", printf("%clalala", '\x00'));
-}
